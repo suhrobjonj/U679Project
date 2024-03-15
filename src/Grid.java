f1cc05fb7af22b01ff85da2b36d3b1cb02c940ca
BT_1N3_16 <wsfdfbk@1jkbndkjf.edf>
2024-03-15 09:10:36 -0400

    Added print statements for SuperTicTacToe Improved Grid

--- src/Grid.java
+++ src/Grid.java
@@ -1,22 +1,52 @@
+
+
 public class Grid {
-    private String[][] grid;
+    private Space[][] grid;
 
     public Grid() {
-        grid = new String[13][13];
+        int counter = 1;
+        int current = 1;
+        grid = new Space[3][13];
         for (int r = 0;r < grid.length;r++) {
             for (int c = 0;c < grid[0].length;c++) {
-                if (r % 4 == 0) {
-                    grid[r][c] = " - ";
-                } else if (c % 4 == 0) {
-                    grid[r][c] = "|";
+                if (c % 4 == 0) {
+                    grid[r][c] = new Space("|");
+                } else {
+                    grid[r][c] = new Space(" " + counter + " ");
+                    counter++;
+                    if (counter - current == 3) {
+                        counter = current;
+                    }
                 }
             }
+            current += 3;
+            counter = current;
         }
-        for (String[] row : grid) {
-            for (String g : row) {
-                System.out.print(g);
+
+        Space[][] row1 = grid;
+        Space[][] row2 = grid;
+        Space[][] row3 = grid;
+
+        for (Space[] row : row1) {
+            for (Space s : row) {
+                System.out.print(s.getSymbol());
             }
             System.out.println();
         }
+        System.out.println();
+        for (Space[] row : row2) {
+            for (Space s : row) {
+                System.out.print(s.getSymbol());
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (Space[] row : row3) {
+            for (Space s : row) {
+                System.out.print(s.getSymbol());
+            }
+            System.out.println();
+        }
+
     }
 }
--- src/SuperTicTacToe.java
+++ src/SuperTicTacToe.java
@@ -1,3 +1,30 @@
+import java.util.Scanner;
+
 public class SuperTicTacToe {
+    private Space[][] row1;
+    private Space[][] row2;
+    private Space[][] row3;
+    private boolean gameOver;
+    public Scanner scan;
+    public SuperTicTacToe () {
+       scan = new Scanner(System.in);
+    }
+
+    public void Play() {
+        System.out.println("Welcome to Super TicTacToe!");
+        System.out.print("Player 1: ");
+        scan.nextLine();
+        System.out.print("Player 2: ");
+        scan.nextLine();
+        System.out.println("Flipping a coin!");
+        System.out.println("Player 1: X");
+        System.out.print("Select the grid: ");
+        while (!gameOver) {
+            System.out.print("Player 1: ");
+            System.out.print("Player 2: ");
+        }
+
+
+    }
 
 }
